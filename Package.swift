d94a756488b1e33feb50dfff215545dbbcf66462
杨俊艺 <1209032479@qq.com>
2022-04-07 10:53:24 +0800

    BabyBluetooth蓝牙库整理成Swift Package方便英勇

--- Package.swift
+++ Package.swift
@@ -6,21 +6,17 @@ import PackageDescription
 let package = Package(
     name: "BabyBluetooth",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "BabyBluetooth",
             targets: ["BabyBluetooth"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "BabyBluetooth",
-            dependencies: []),
+        .target(name: "BabyBluetooth", dependencies: [], path: "Sources/BabyBluetooth",
+                exclude: [],
+                resources: [],
+                publicHeadersPath: "",
+                cSettings: []),
         .testTarget(
             name: "BabyBluetoothTests",
             dependencies: ["BabyBluetooth"]),
--- Sources/BabyBluetooth/BabyBluetooth.swift
+++
@@ -1,6 +0,0 @@
-public struct BabyBluetooth {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
